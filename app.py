c36c4c8b68d9dbe1a779ad5c4ae32ebcd25f13bc
Mona Lisa <ridhish@kapsicumweb.com>
2021-12-23 08:22:33 +0530

    Image Validation Removed

--- app.py
+++ app.py
@@ -14,20 +14,24 @@ class Students(db.Model):
     phone = db.Column(db.Integer)
     pic = db.Column(db.String)
 
-class Stud(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    surname = db.Column(db.String(100))
-    cell = db.Column(db.Integer)
+#----------------------------- Home Starts Here -----------------------------------
+@app.route('/', methods=["GET","POST"])
+def index():
+    if request.method=="POST":
+        name = request.form["name"]
+        phone = request.form["phone"]
+        file = request.files['pic']
+        fname= 'static/images/photos/'+secure_filename(file.filename)
+        file.save(fname)
+        info=Students(name=name, phone=phone, pic=fname)
+        db.session.add(info)
+        db.session.commit()
+    fetch=Students.query.all()
 
+    return render_template("index.html", getinfo=fetch)
+#----------------------------- Home Ends Here -----------------------------------
 
-
-@app.route('/delete/<int:id>', methods=["GET","POST"])
-def delete(id):
-    fetch=Students.query.filter_by(id=id).first()
-    db.session.delete(fetch)
-    db.session.commit()
-    return redirect("/")
-
+#----------------------------- Update Starts Here -----------------------------------
 @app.route('/updater', methods=["GET","POST"])
 def updater():
     if request.method=="POST":
@@ -38,14 +42,9 @@ def updater():
         fetch.name=name
         fetch.phone=phone
         file = request.files['pic']
-        file.seek(0, os.SEEK_END)
-        if file.tell() == 0:
-            pass
-        else:
-            file.seek(0)
-            fname= 'static/images/photos/'+secure_filename(file.filename)
-            file.save(fname)
-            fetch.pic=fname
+        fname= 'static/images/photos/'+secure_filename(file.filename)
+        file.save(fname)
+        fetch.pic=fname
         db.session.commit()
         return redirect("/")
 
@@ -55,24 +54,16 @@ def update():
     fetch=Students.query.filter_by(id=id).all()
     return render_template("update.html",getinfo=fetch)
 
-@app.route('/', methods=["GET","POST"])
-def index():
-    if request.method=="POST":
-        name = request.form["name"]
-        file = request.files["pic"]
-        phone = request.form["phone"]
-        file = request.files['pic']
-        file.seek(0, os.SEEK_END)
-        if file.tell() == 0:
-            pass
-        file.seek(0)
-        fname= 'static/images/photos/'+secure_filename(file.filename)
-        file.save(fname)
-        info=Students(name=name, phone=phone, pic=fname)
-        db.session.add(info)
-        db.session.commit()
-    fetch=Students.query.all()
+#----------------------------- Update Ends Here -----------------------------------
 
-    return render_template("index.html", getinfo=fetch)
+#----------------------------- Delete Starts Here -----------------------------------
+@app.route('/delete/<int:id>', methods=["GET","POST"])
+def delete(id):
+    fetch=Students.query.filter_by(id=id).first()
+    db.session.delete(fetch)
+    db.session.commit()
+    return redirect("/")
+#----------------------------- Delete Ends Here -----------------------------------
+
 if __name__ == '__main__':
    app.run(debug=True)
